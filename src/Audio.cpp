95f6a06c4eb37cfa0e0969ef8c127fb2cb830475
vofes <bielovlad@gmail.com>
2023-10-16 20:03:28 +0200

    Base(withoud sound)

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -3,29 +3,95 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
 
-Audio::Audio() : m_engine()
-{
-    ma_result result;
+std::vector<Audio::AudioSource*> Audio::s_sources = {};
 
-    result = ma_engine_init(NULL, &m_engine);
-    if (result != MA_SUCCESS)
+Audio::Audio(const char* wavFilePath) : m_audioFileData(nullptr), m_fileSize(0)
+{
+    Audio::s_sources.reserve(10);
+    // Open the .wav file
+    FILE* pFile = fopen(wavFilePath, "rb");
+    if (pFile == NULL) 
     {
-        std::cout << "Error: miniaudio was not inited" << std::endl;
+        std::cerr << "Audio Error: File open error!" << std::endl;
+        fclose(pFile);
         return;
     }
+
+    // Get the file size
+    fseek(pFile, 0, SEEK_END);
+    m_fileSize = ftell(pFile);
+    fseek(pFile, 0, SEEK_SET);
+
+    // Read the file contents into a buffer
+    m_audioFileData = malloc(m_fileSize);
+    if (m_audioFileData == NULL) 
+    {
+        std::cerr << "Audio Error: Memory allocation error!" << std::endl;
+        m_audioFileData = nullptr;
+        fclose(pFile);
+        return;
+    }
+
+    fread(m_audioFileData, 1, m_fileSize, pFile);
+    fclose(pFile);
 }
 
-Audio::~Audio()
+void Audio::Play() 
 {
-    ma_engine_uninit(&m_engine);
+    AudioSource* source = new AudioSource(m_audioFileData, m_fileSize);
+    s_sources.push_back(source);
 }
 
-void Audio::Play(const char* wavFile)
+Audio::~Audio() 
 {
-    ma_engine_play_sound(&m_engine, wavFile, NULL);
+    for(int i = 0; i < s_sources.size(); i++)
+    {
+        delete s_sources[i];
+    }
+    s_sources.clear();
+    // free memory
 }
 
-void Audio::Stop()
+void Audio::DataCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) 
 {
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == nullptr)
+        return;
 
+    ma_uint64 frameCurser;
+    ma_decoder_get_cursor_in_pcm_frames(pDecoder, &frameCurser);
+    ma_uint64 framesLength;
+    ma_decoder_get_length_in_pcm_frames(pDecoder, &framesLength);
+    if(frameCurser >= framesLength)
+    {
+        int size = s_sources.size();
+        int it = FindIterator(pDevice);
+        std::cout << "End" << std::endl;
+        if(it != -1)
+            s_sources.erase(s_sources.begin() + it);
+    }
+    
+    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, nullptr);
+    (void)pInput; // Unused
+
+    // Apply volume
+    // float* pSamples = (float*)pOutput;
+    // for (ma_uint32 i = 0; i < frameCount * pDevice->playback.channels; ++i) 
+    // {
+    //     pSamples[i] *= pDecoder->volume;
+    // }
+
+}
+
+int Audio::FindIterator(ma_device *pDevice)
+{
+    for(int i = 0; i < s_sources.size(); i++)
+    {
+        if (&(s_sources[i]->m_device) == pDevice) 
+        {
+            // delete s_sources[i];
+            return i;
+        }
+    }
+    return -1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,39 +8,36 @@ int main()
     Window window = Window("Window", 720, 480, true);
     Input input = Input(window.GetGLFW());
     OpenGL openGL = OpenGL();
-
+    Audio audio = Audio("res/game_over.wav");
     Image testUV = Image("res/test.png");
-    Image slime = Image("res/slime.png");
-    Image button = Image("res/blue_button.png");
 
-    Audio audio = Audio();
-    audio.Play("res/game_over.wav");
+
     
     // Render loop
-    int i = 0;
     while (window.IsOpen())
     {
         openGL.FillBackground(0x300a24);
-        
-        openGL.DrawImage(&slime, 0, 0, 16, 16, -1, -1, 1, 1);
-        openGL.DrawImage(&button, 0, 0, 190, 49, -1, 0, 1, 49.0f / 190.0f);
-        openGL.DrawImage(&testUV, 0, 0, 4096, 4096, 0, 0, 1, 1);
-        openGL.DrawRect((float)i / 100.0f, 0, 0.5f, 0.5f, 0xa5a8e6);
-        openGL.DrawRect(-1.0f, -1.0f, 0.5f, 0.5f, 0x327da8);
+        openGL.DrawImage(&testUV, 0, 0, 4096, 4096, -0.5f, -0.5f, 1, 1);
 
+        // Debug with ImGui
         ImGui::SetNextWindowPos(ImVec2(0, 0));
         ImGui::SetNextWindowSize(ImVec2(200, 60));
         if(ImGui::Begin("Debug", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBackground))
         {
-            ImGui::TextUnformatted(("fps: " + std::to_string(window.fps)).c_str());
             ImGui::TextUnformatted(("window: " + std::to_string(Window::width) + "x" + std::to_string(Window::height)).c_str());
+            ImGui::TextUnformatted(("fps: " + std::to_string(window.fps)).c_str());
+            ImGui::TextUnformatted(("sounds: " + std::to_string(audio.s_sources.size())).c_str());
         }
         ImGui::End();
 
+        // Check input
         if(Input::IsKeyPressed(GLFW_KEY_W))
         {
-            i++;
+            audio.Play();
         }
+
+        if(Input::IsKeyPressed(GLFW_KEY_ESCAPE))
+            return 0;
 
         openGL.Render();
         window.EndFrame();
